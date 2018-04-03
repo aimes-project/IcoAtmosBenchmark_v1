d623503a39a236c47e2c37bb1d85342499e74662
Hisashi Yashiro <h.yashiro@riken.jp>
2018-04-03 18:56:09 +0900

    Add more reference output of DYNAMICO kernels

--- kernels/DYNAMICO/comp_caldyn_vert/run/run-Mistral.sh
+++ kernels/DYNAMICO/comp_caldyn_vert/run/run-Mistral.sh
@@ -1,4 +1,14 @@
 #! /bin/bash
+#
+# for DKRZ Mistral
+#
+#SBATCH --partition=compute
+#SBATCH --account=ku0598
+#SBATCH --job-name=IABknl
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --time=00:10:00
+set -e
 
 export OMP_NUM_THREADS=1
 
